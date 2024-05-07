a050d1e1b9f577bd25e4bdd28a35c0c248b508ed
Antonio Ignacio Campos Ruiz <acamposruiz@gmail.com>
2024-05-07 17:01:21 +0200

    Refactor import statements in app.controller.ts and app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World! Huuuray!!!';
+  async getHello(): Promise<string> {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('Hello World! Huuuray!!!');
+      }, 1000);  // Delay de 1000 milisegundos (1 segundo)
+    });
   }
 
   getPing(): string {
