3cd329d3991d38aaec3516bff4c6fbd0928abac7
Antonio Ignacio Campos Ruiz <acamposruiz@gmail.com>
2024-05-09 15:21:35 +0200

    Update delay in AppService to improve performance

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ export class AppService {
     return new Promise(resolve => {
       setTimeout(() => {
         resolve('Hello World! Huuuray!!!');
-      }, 3400);  // Delay de 1000 milisegundos (1 segundo)
+      }, 0);  // Delay de 1000 milisegundos (1 segundo)
     });
   }
 
