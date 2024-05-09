ff5f11ec1359ac09ab58e6382f8598e340926236
Antonio Ignacio Campos Ruiz <acamposruiz@gmail.com>
2024-05-09 23:56:08 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ export class AppService {
     return new Promise(resolve => {
       setTimeout(() => {
         resolve('Hello World! Huuuray!!!');
-      }, 300);  // Delay de 1000 milisegundos (1 segundo)
+      }, 400);  // Delay de 1000 milisegundos (1 segundo)
     });
   }
 
